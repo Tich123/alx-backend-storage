0619b41d1eb01a17472b57df033c56bd29bff70c
Tich123 <mashavatich@gmail.com>
2024-05-15 10:33:32 +0000

    red

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
-
+"""
+web cache and tracker
+"""
 import requests
-import time
 import redis
+from functools import wraps
 
-# Initialize Redis connection
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+store = redis.Redis()
 
+
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
 def get_page(url: str) -> str:
-    cache_key = f'page:{url}'
-    count_key = f'count:{url}'
-
-    # Check if the page is cached
-    cached_page = redis_client.get(cache_key)
-    if cached_page:
-        # Increment the access count
-        redis_client.incr(count_key)
-        return cached_page
-
-    # Fetch the page content
-    response = requests.get(url)
-    page_content = response.text
-
-    # Cache the page with expiration time of 10 seconds
-    redis_client.setex(cache_key, 10, page_content)
-
-    # Track the access count
-    redis_client.incr(count_key)
-
-    return page_content
-
-# Test the function
-sample_url = 'http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.co.uk'
-print(get_page(sample_url))
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
